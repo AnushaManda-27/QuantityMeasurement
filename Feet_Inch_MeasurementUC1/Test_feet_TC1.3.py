042a1f5df5c54d898216d2806a820a0acea64e67
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-17 20:21:56 +0530

    [Anusha] Add: check for type of value

---
+++ Feet_Inch_MeasurementUC1/Test_feet_TC1.3.py
@@ -0,0 +1,37 @@
+'''@Author: Anusha Manda
+@Date: 16-08-2021 12: 00: 30
+@Last Modified by: Anusha Manda
+@Last Modified time: 16-08-2021 18: 00: 30
+@Title: Test Feet equal to conversion
+'''
+
+import unittest
+from Quantity_Feet import Feet_Quantity
+
+
+class TestQuantity(unittest.TestCase):
+
+    def test_feet_equalto_feet(self):
+        '''Description: test_feet_equalto_feet is defined to check if feet equal to feet
+            Function Parameters: self
+            Return: None'''
+        quantity = Feet_Quantity(12, 12)
+        res = quantity.feet_Equal_feet()
+        self.assertEqual(res, 'Equal')
+
+    def test_feet_equalto_feet_Null_check(self):
+        '''Description: test_feet_equalto_feet_Null_check is defined to check if feet not none
+            Function Parameters: self
+            Return: None'''
+        quantity = Feet_Quantity(0, 0)
+        res = quantity.feet_Not_None()
+        self.assertEqual(res, 'Not None')
+
+    def test_feet_equalto_feet_reference_check(self):
+        '''Description: test_feet_equalto_feet_Reference_check is defined to check if reference is equal
+            Function Parameters: self
+            Return: None'''
+        #quant = 30
+        quantity = Feet_Quantity(10, 30)
+        res = self.assertIsInstance(quantity, Feet_Quantity)
+        self.assertEqual(res, None)
