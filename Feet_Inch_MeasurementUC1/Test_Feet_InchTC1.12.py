4d3ffd92afed2bf9f101c81f9b49ac120c3ce5ca
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-17 21:46:27 +0530

    [Anusha] Add: 1 inch is not equal to in 1feet

---
+++ Feet_Inch_MeasurementUC1/Test_Feet_InchTC1.12.py
@@ -0,0 +1,50 @@
+'''@Author: Anusha Manda
+@Date: 16-08-2021 12: 00: 30
+@Last Modified by: Anusha Manda
+@Last Modified time: 16-08-2021 18: 00: 30
+@Title: Test Feet to Inch conversion
+'''
+
+from Feet_Inch_conv import Inches_Feet
+import unittest
+from Feet_Inch_conv import Feet_Inches
+
+
+class feet_inches_test(unittest.TestCase):
+
+    def test_12feet_1inches(self):
+        '''Description: the function test_12feet_1inches is defined to check
+            Function Arguments: self
+            Return: None'''
+        feet_inches = Feet_Inches(1, 12)
+        res = feet_inches.feet_to_inches_check()
+        self.assertEqual(res, True)
+
+    def test_1feet_NotEqual_1Inch(self):
+        '''Description: the function test_1feet_NotEqual_1Inch is defined to check
+            Function Arguments: self
+            Return: None'''
+        feet_inch = Feet_Inches(1, 1)
+        res = feet_inch.feet_to_inches_check()
+        self.assertEqual(res, False)
+
+    def test_12inches_1feet(self):
+        '''Description: the function test_12inches_1feet is defined to check
+            Function Arguments: self
+            Return: None'''
+
+        inches_feet = Inches_Feet(12, 1)
+        res = inches_feet.inches_to_feet_check()
+        self.assertEqual(res, True)
+
+    def test_1inches_Not_1feet(self):
+        '''Description: the function test_1inches_Not_1feet is defined to check
+            Function Arguments: self
+            Return: None'''
+        inches_feet = Inches_Feet(1, 1)
+        res = inches_feet.inches_to_feet_check()
+        self.assertEqual(res, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
