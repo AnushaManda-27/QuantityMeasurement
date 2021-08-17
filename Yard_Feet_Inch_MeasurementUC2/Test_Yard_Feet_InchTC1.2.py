74087ee1e87e5e28cb028fcf338a219ce54f70ae
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-17 22:08:11 +0530

    [Anusha] Add: check 1 feet not equal to1 yard

---
+++ Yard_Feet_Inch_MeasurementUC2/Test_Yard_Feet_InchTC1.2.py
@@ -0,0 +1,28 @@
+'''@Author: Anusha Manda
+@Date: 16-08-2021 12: 00: 30
+@Last Modified by: Anusha Manda
+@Last Modified time: 16-08-2021 18: 00: 30
+@Title: Test Feet_Inch_Yard conversion
+'''
+import unittest
+
+from yard_feet import Yard_Feet, Feet_Yard, Yard_inch, Inch_Yard
+
+
+class TestYardToFeet(unittest.TestCase):
+
+    def test_3feet_1yard(self):
+        '''Description: the function test_3feet_1yard is defined to check if it true
+            Function Arguments: self
+            Return: None'''
+        yard_feet = Yard_Feet(3, 1)
+        res = yard_feet.yard_to_feet_check()
+        self.assertEqual(res, True)
+
+    def test_1feet_NotEqual_1yard(self):
+        '''Description: the function test_1feet_NotEqual_1yard is defined to check if it is False
+            Function Arguments: self
+            Return: None'''
+        yard_feet = Yard_Feet(1, 1)
+        res = yard_feet.yard_to_feet_check()
+        self.assertEqual(res, False)
