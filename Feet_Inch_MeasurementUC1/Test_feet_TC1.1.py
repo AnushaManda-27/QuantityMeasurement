1c055b214f01ef9ecf851cf47b0cda59c0f49188
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-17 19:59:35 +0530

    [Anusha] Add: check for feet equal to feet

---
+++ Feet_Inch_MeasurementUC1/Test_feet_TC1.1.py
@@ -0,0 +1,20 @@
+'''@Author: Anusha Manda
+@Date: 16-08-2021 12: 00: 30
+@Last Modified by: Anusha Manda
+@Last Modified time: 16-08-2021 18: 00: 30
+@Title: Test Feet equal to conversion
+'''
+
+import unittest
+from Quantity_Feet import Feet_Quantity
+
+
+class TestQuantity(unittest.TestCase, Exception):
+
+    def test_feet_equalto_feet(self):
+        '''Description: test_feet_equalto_feet is defined to check if feet equal to feet
+            Function Parameters: self
+            Return: None'''
+        quantity = Feet_Quantity(12, 12)
+        res = quantity.feet_Equal_feet()
+        self.assertEqual(res, 'Equal')
