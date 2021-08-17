c9c3d38800e81162ff6260482f5ce161b0e6bcd7
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-17 20:08:03 +0530

    [Anusha] Add: check if feet not None

---
+++ Feet_Inch_MeasurementUC1/Test_feet_TC1.2.py
@@ -0,0 +1,28 @@
+'''@Author: Anusha Manda
+@Date: 16-08-2021 12: 00: 30
+@Last Modified by: Anusha Manda
+@Last Modified time: 16-08-2021 18: 00: 30
+@Title: Test Feet equal to conversion
+'''
+
+import unittest
+from Quantity_Feet import Feet_Quantity
+
+
+class TestQuantity(unittest.TestCase, Exception):
+
+    def test_feet_equalto_feet(self):
+        '''Description: test_feet_equalto_feet is defined to check if feet equal to feet
+            Function Parameters: self
+            Return: None'''
+        quantity = Feet_Quantity(12, 12)
+        res = quantity.feet_Equal_feet()
+        self.assertEqual(res, 'Equal')
+
+    def test_feet_equalto_feet_Null_check(self):
+        '''Description: test_feet_equalto_feet_Null_check is defined to check if feet not none
+            Function Parameters: self
+            Return: None'''
+        quantity = Feet_Quantity(0, 0)
+        res = quantity.feet_Not_None()
+        self.assertEqual(res, 'Not None')
