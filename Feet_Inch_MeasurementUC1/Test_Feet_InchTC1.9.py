3f3bab6a600352544799580b2794f6e6b5714a73
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-17 21:38:40 +0530

    [Anusha] Add: given 1 feet equals 12 inches

---
+++ Feet_Inch_MeasurementUC1/Test_Feet_InchTC1.9.py
@@ -0,0 +1,21 @@
+'''@Author: Anusha Manda
+@Date: 16-08-2021 12: 00: 30
+@Last Modified by: Anusha Manda
+@Last Modified time: 16-08-2021 18: 00: 30
+@Title: Test Feet to Inch conversion
+'''
+
+from Feet_Inch_conv import Inches_Feet
+import unittest
+from Feet_Inch_conv import Feet_Inches
+
+
+class feet_inches_test(unittest.TestCase):
+
+    def test_12feet_1inches(self):
+        '''Description: the function test_12feet_1inches is defined to check
+            Function Arguments: self
+            Return: None'''
+        feet_inches = Feet_Inches(1, 12)
+        res = feet_inches.feet_to_inches_check()
+        self.assertEqual(res, True)
